9cc521ec7b4243cdd17d1b54cac058540ad38e8a
Jean-Michel Bodart <jbodart@argenx.com>
2022-09-17 22:53:57 +0200

    submit programs to SASjs with XMLHttpRequest

--- extension.js
+++ extension.js
@@ -5,11 +5,50 @@ var cp = require('child_process');
 var os = require('os');
 var fs = require('fs');
 var path = require('path');
+const { debug } = require('console');
+//var XMLHttpRequest = require('XMLHttpRequest');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const sasLogChannel = vscode.window.createOutputChannel("SASLog");
+var statusBarItem;
 
 let NEXT_TERM_ID = 1;
 const platform = os.platform();
 //const userHomeDir = os.homedir();
 
+function setupLogViewer() {
+    statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, -1);
+    statusBarItem.text = 'SAS Log';
+    statusBarItem.command = 'SASLog.open-output';
+    //toggleStatusBarItem(vscode.window.activeTextEditor);
+    //outputChannel = vscode.window.getOutputChannel('SASLog');
+    return vscode.commands.registerCommand('SASLog.open-output', () => {
+        sasLogChannel.show();
+    });
+}
+
+function customLsafResults( text, 
+                            search = '^NOTE: SAS Life Science Analytics Framework Macro:',
+                            replace = 'NOTE: LSAF Macro:'
+                            ){
+    console.log("(customLsafResults): search="+search);
+    let textByLine = text.split("\n");
+    let rx =  RegExp(search,'i');
+    let textFiltered = textByLine.filter(line => rx.test(line));
+    if (textFiltered.length > 0) {
+        return textFiltered.join("\n");
+    } else {
+        return textByLine.slice(Math.max(1,textByLine.length-20)).join("\n");
+    }
+}
+
+function tail(text, n=6){
+    let textByLine = text.split("\n");
+    if (textByLine.length > n) {
+        return textByLine.slice(textByLine.length-n).join("\n");
+    } else {
+        return textByLine.join("\n");
+    }
+}
 
 function revealFileInOs(context){
     console.log('Platform: '+platform);
@@ -42,6 +81,16 @@ function Lsaf_Local_Root(){
     }
     return lsaf_root_path;
 }
+
+function httpGetAsync(theUrl, callback) {
+    let xmlHttpReq = new XMLHttpRequest();
+    xmlHttpReq.onreadystatechange = function () {
+      if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200)
+        callback(xmlHttpReq.responseText);
+    }
+    xmlHttpReq.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttpReq.send(null);
+  }
 
 function executeCustomFileCommand(command, context) {
     console.log("Local path: " + context.fsPath); // c:\Users\jbodart\VSXproj\OpenFolderInExplorer\img\inAction.gif
@@ -113,11 +162,116 @@ function executeCustomFileCommand(command, context) {
 				//console.error("Could not remove Lsaf Local Root Folder ("+lsaf_root_path+") from Unix Path: "+ux_path);
 				console.error("File Path: "+ux_path+" is not within Lsaf Local Root Folder ("+lsaf_root_path+")");
 			}
+        } else if (command === "copyToLSAF2"){
+            let ux_path = context.fsPath.replace(/\\/g, "/");
+            let path_elements = ux_path.split("/");
+            let fname = path_elements[path_elements.length-1];
+			//console.log("ux_path: "+ux_path);
+            //console.log("fname: "+fname);
+            let lsaf_root_path = Lsaf_Local_Root();
+            //console.log("lsaf_root_path: "+lsaf_root_path);
+            let lsafLRF_pattern = new RegExp("^"+lsaf_root_path, 'i');
+            if (lsafLRF_pattern.test(ux_path)) {
+				let lsaf_path = ux_path.replace(lsafLRF_pattern, "");
+				console.log("LSAF Path: "+ lsaf_path);	
+				console.log("\nWaiting for User comment... ");	
+                vscode.window.showInputBox({ 
+                    prompt: 'Enter version comment (this enables LSAF versioning)' ,
+                    title: "Copy to lSAF", 
+                    value: "Update "+fname
+                    }).then((comment) => {
+                        console.log("comment: "+ comment + "\n");
+                        let execURL = "http://localhost:5000/SASjsApi/stp/execute"
+                                     +"?_program="
+                                     //+"/test/test.sas"
+                                     +"/general/biostat/tools/Copy_to_lsaf.sas"
+                                     +"&_debug=131"
+                                     +"&param1=my-value-1"
+                                     +"&local_path="+encodeURIComponent(context.fsPath)
+                                     +"&message="+encodeURIComponent(comment)
+                                     ;
+                        console.log("Sending GET request: \n"+execURL)
+                        httpGetAsync(execURL, function(result){
+                            console.log("\n=== SAS RESULTS ===\n");
+                            let filtered_result = customLsafResults(result, '^NOTE\\: SAS Life Science Analytics Framework Macro\\: \\*  The file ');
+                            console.log(filtered_result);                            
+                            vscode.window.showInformationMessage(tail(filtered_result, 1));
+                            console.log("\n=== END OF SAS RESULTS ===\n");                            
+                        });
+                        
+                    });
+			} else {
+				//console.error("Could not remove Lsaf Local Root Folder ("+lsaf_root_path+") from Unix Path: "+ux_path);
+				console.error("File Path: "+ux_path+" is not within Lsaf Local Root Folder ("+lsaf_root_path+")");
+			}
         } else {
             cp.execSync(`${command} "${context.fsPath}"`);
             console.log("Done with "+command);
         }
         return;
+    } else if (command.toString().indexOf("SASjs") > -1) {
+        console.log("SASjs Command: "+command);        
+        if (command === "SASjs run"){
+            let ux_path = context.fsPath.replace(/\\/g, "/");
+            let path_elements = ux_path.split("/");
+            let fname = path_elements[path_elements.length-1];
+            let fname_elemnts = fname.split('.');
+            let fext = fname_elemnts[fname_elemnts.length-1];
+            if (! fext === "sas") {
+                return vscode.window.showErrorMessage("SASjs run: Invalid file extension: "+fext
+                +" in submitted file: "+context.fsPath);            
+            }
+			//console.log("ux_path: "+ux_path);
+            //console.log("fname: "+fname);
+            let lsaf_root_path = Lsaf_Local_Root();
+            //console.log("lsaf_root_path: "+lsaf_root_path);
+            let lsafLRF_pattern = new RegExp("^"+lsaf_root_path, 'i');
+            if (lsafLRF_pattern.test(ux_path)) {
+				let lsaf_path = ux_path.replace(lsafLRF_pattern, "");
+				console.log("LSAF Path: "+ lsaf_path);	
+				let execURL = "http://localhost:5000/SASjsApi/stp/execute"
+                                     +"?_program="
+                                     //+"/test/test.sas"
+                                     +lsaf_path
+                                     +"&_debug=131"
+                                     +"&local_path="+encodeURIComponent(context.fsPath)
+                                     ;
+                console.log("Sending GET request: \n"+execURL)
+                httpGetAsync(execURL, function(result){
+                        console.log("\n=== SAS RESULTS: "+fname+" ===\n");
+                        let filtered_result = customLsafResults(result, '^(ERROR( \\d+)?|WARNING):');
+                        setupLogViewer();
+                        sasLogChannel.appendLine("\n=== SAS Log: "+fname+" ===\n");
+                        sasLogChannel.append(result);
+                        sasLogChannel.appendLine("\n=== END OF SAS Log: "+fname+" ===\n");
+                        sasLogChannel.show();
+                        console.log(filtered_result);    
+                        let header = 'Program: '+fname;
+                        let options = { detail: filtered_result, modal: true };
+                        //vscode.window.showInformationMessage(header, options, ...["Ok"]).then((item)=>{
+                        //    console.log(item);
+                        //});
+                        if (/(^|\n)ERROR( \d+)?\:/.test(filtered_result))  {
+                            let header = 'Program ran with ERRORS: '+fname;
+                            vscode.window.showErrorMessage(filtered_result);
+                            vscode.window.showErrorMessage(header, options);
+                        } else if (/(^|\n)WARNING\:/.test(filtered_result))  {
+                            let header = 'Program ran with WARNINGS: '+fname;
+                            vscode.window.showWarningMessage(filtered_result);
+                            vscode.window.showWarningMessage(header, options);
+                        } else {
+                            options = { detail: tail(filtered_result, 10), modal: true };
+                            vscode.window.showInformationMessage(header, options, ...["Ok"]);                        
+                            vscode.window.showInformationMessage(tail(filtered_result, 10));
+                        }                    
+                        console.log("\n=== END OF SAS RESULTS ===\n");                            
+                    });
+            } else {
+                console.error("File Path: "+ux_path+" is not within Lsaf Local Root Folder ("+lsaf_root_path+")");
+            }
+        } else {
+            debug.warn("Unrecognized SASjs command: "+command);
+        }
     } else if (command.toString() === "revealFileInOs") {
         console.log("Executing Custom Command: extension."+command.toString());
         revealFileInOs(context)
